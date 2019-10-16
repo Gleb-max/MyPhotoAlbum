9a5005ef5583abd1ee4c7455a988db7c4862495e
Gleb-max <52007123+Gleb-max@users.noreply.github.com>
2019-10-16 17:05:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
+INDEX = 1
+
+
+def image_path(name):
+    global INDEX
+    name = secure_filename(name)
+    if len(name.split('.')) == 1:
+        name = f'{INDEX}.{name}'
+        INDEX += 1
+    return f'Images/{name}'
 
 
 @app.route('/show/images/')
@@ -15,9 +25,14 @@ def show_images():
 def add_image():
     if request.method == 'POST':
         file = request.files['photo']
-        file.save('Images/' + secure_filename(file.filename))
-        return 'Successfully'
+        file.save(image_path(file.filename))
+        return render_template('show-image.html', names=os.listdir('Images'))
     return render_template('upload-image.html')
+
+
+@app.route('/open/<filename>')
+def open_image(filename):
+    return send_from_directory('Images', filename)
 
 
 if __name__ == '__main__':
