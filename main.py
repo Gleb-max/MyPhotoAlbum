bedc7dbe18882cb1abe1c26361c499683c52fa7d
Gleb-max <52007123+Gleb-max@users.noreply.github.com>
2019-10-20 20:30:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ IMAGE_FOLDER = 'img/'
 MINIATURES_FOLDER = 'min/'
 
 
-def image_name(name):
-    global INDEX
-    name = secure_filename(name)
-    data = name.split('.')
-    if len(data) == 1:
-        name = f'{INDEX}.{name}'
+def image_name(name):    # возвращает безопасное имя для загружаемой картинки
+    global INDEX         # функция secure_filename возвращает только расширение файла
+    name = secure_filename(name)    # в случае "опасности" имени,
+    data = name.split('.')          # поэтому такие картинки сохраняю под числовым именем,
+    if len(data) == 1:              # попутно следя, чтобы картинки не "пересохранялись"
+        name = f'{INDEX}.{name}'    # (в случае числового имени загружаемой картинки)
         INDEX += 1
     elif len(data) == 2 and data[0].isdigit():
         name = f'{INDEX}.{data[1]}'
@@ -24,21 +24,21 @@ def image_name(name):
     return name
 
 
-def create_miniature(filename):
+def create_miniature(filename):     # создание миниатюры
     Image.open(IMAGE_FOLDER + filename).resize((500, 500), Image.ADAPTIVE).save(MINIATURES_FOLDER + filename)
 
 
-@app.route('/gallery/', methods=['GET', 'POST'])
+@app.route('/gallery/', methods=['GET', 'POST'])    # единственная страница (на ней и галерея и загрузка)
 def show_images():
     if request.method == 'POST':
         file = request.files['photo']
         filename = image_name(file.filename)
         file.save(IMAGE_FOLDER + filename)
-        Thread(target=lambda: create_miniature(filename)).start()
-    return render_template('show-image.html', names=os.listdir('min'))
+        Thread(target=lambda: create_miniature(filename)).start()   # создание миниатюры в отдельном потоке
+    return render_template('show-image.html', names=os.listdir('min'))  # для ускорения ответа
 
 
-@app.route('/open/<directory>/<filename>')
+@app.route('/open/<directory>/<filename>')   # открытие файла, указанного в пути <directory>/<filename>
 def open_file(directory, filename):
     return send_from_directory(directory, filename)
 
