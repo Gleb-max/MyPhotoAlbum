7e28da2900989094f771f7a02611a669f821d102
Gleb-max <52007123+Gleb-max@users.noreply.github.com>
2020-01-13 14:02:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,24 +50,10 @@ def open_file(directory, filename):
 def delete_all():
     folder = 'min'
     for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            pass
+        os.remove(os.path.join(folder, filename))
     folder = 'img'
     for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            pass
+        os.remove(os.path.join(folder, filename))
 
 
 if __name__ == '__main__':
